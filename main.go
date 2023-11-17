e6813134178cdd4e778678bf464245d283180f8b
satriahelmy <helmysmp@gmail.com>
2023-11-17 20:33:11 +0700

    hello world

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/hello", helloHandler)
+	log.Println("Starting web on port 8080")
+
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Welcome to home"))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world"))
+}
